bc59ee4fee3a9081329b820df304ffd59e1baeb6
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-01-23 13:04:03 +0100

    Remove reflector component

--- src/phantasm-hardware-interface/d3d12/common/util.hh
+++ src/phantasm-hardware-interface/d3d12/common/util.hh
@@ -5,8 +5,6 @@
 #include <clean-core/capped_vector.hh>
 
 #include <phantasm-hardware-interface/types.hh>
-
-#include <phantasm-hardware-interface/assets/vertex_attrib_info.hh>
 #include <phantasm-hardware-interface/d3d12/common/d3d12_sanitized.hh>
 
 namespace phi::d3d12::util
