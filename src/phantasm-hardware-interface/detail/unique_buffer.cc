329ea6e53f8aa40fe9832746564bce2a8d7c998e
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-06-03 19:33:42 +0200

    Add unique_buffer::write_to_binary_file

--- src/phantasm-hardware-interface/detail/unique_buffer.cc
+++ src/phantasm-hardware-interface/detail/unique_buffer.cc
@@ -13,3 +13,14 @@ phi::detail::unique_buffer phi::detail::unique_buffer::create_from_binary_file(c
     file.read(res.get_as_char(), long(size));
     return res;
 }
+
+bool phi::detail::unique_buffer::write_to_binary_file(const char* filename)
+{
+    std::ofstream file(filename, std::ios::binary);
+    if (!file.good())
+        return false;
+
+    file.write(get_as_char(), size());
+    file.close();
+    return !file.fail();
+}
