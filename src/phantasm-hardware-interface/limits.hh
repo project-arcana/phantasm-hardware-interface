16f7f031408b38429b6c816327f8cddb53537842
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-06-06 21:07:13 +0200

    Increase root constant size from 8 to 16

--- src/phantasm-hardware-interface/limits.hh
+++ src/phantasm-hardware-interface/limits.hh
@@ -21,7 +21,7 @@ inline constexpr unsigned max_shader_samplers = 16u;
 
 /// the maximum size for root constants
 /// configurable in increments of sizeof(DWORD32)
-inline constexpr unsigned max_root_constant_bytes = 8u;
+inline constexpr unsigned max_root_constant_bytes = 16u;
 
 /// the maximum amount of argument associations
 /// configurable
