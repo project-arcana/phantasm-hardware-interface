e2a375e6a19d9ec572a6b92d261627054a5d8322
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-09-02 22:54:37 +0200

    Fix d3d12 fence shutdown

--- src/phantasm-hardware-interface/d3d12/Fence.cc
+++ src/phantasm-hardware-interface/d3d12/Fence.cc
@@ -7,33 +7,33 @@
 
 void phi::d3d12::SimpleFence::initialize(ID3D12Device& device)
 {
-    CC_ASSERT(!mFence.is_valid());
-    mEvent = CreateEventEx(nullptr, FALSE, FALSE, EVENT_ALL_ACCESS);
-    CC_ASSERT(mEvent != INVALID_HANDLE_VALUE && "failed to create win32 event");
-    PHI_D3D12_VERIFY(device.CreateFence(0, D3D12_FENCE_FLAG_NONE, PHI_COM_WRITE(mFence)));
+    CC_ASSERT(fence == nullptr && "double init");
+    event = CreateEventEx(nullptr, FALSE, FALSE, EVENT_ALL_ACCESS);
+    CC_ASSERT(event != INVALID_HANDLE_VALUE && "failed to create win32 event");
+    PHI_D3D12_VERIFY(device.CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence)));
 }
 
 void phi::d3d12::SimpleFence::destroy()
 {
-    if (mFence.is_valid())
+    if (fence != nullptr)
     {
-        mFence = nullptr;
-        ::CloseHandle(mEvent);
+        fence->Release();
+        fence = nullptr;
+        ::CloseHandle(event);
     }
 }
 
-
 void phi::d3d12::SimpleFence::waitCPU(uint64_t val)
 {
-    if (mFence->GetCompletedValue() <= val)
+    if (fence->GetCompletedValue() <= val)
     {
-        PHI_D3D12_VERIFY(mFence->SetEventOnCompletion(val, mEvent));
-        ::WaitForSingleObject(mEvent, INFINITE);
+        PHI_D3D12_VERIFY(fence->SetEventOnCompletion(val, event));
+        ::WaitForSingleObject(event, INFINITE);
     }
 }
 
 void phi::d3d12::SimpleFence::waitGPU(uint64_t val, ID3D12CommandQueue& queue)
 {
     //
-    PHI_D3D12_VERIFY(queue.Wait(mFence, val));
+    PHI_D3D12_VERIFY(queue.Wait(fence, val));
 }
