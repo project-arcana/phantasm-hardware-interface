d5db3ad4007f8adde62ade360ac323aa7f775b74
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2021-08-17 17:27:49 +0200

    Fix D3D12 shader table root const ordering

--- src/phantasm-hardware-interface/d3d12/common/safe_seh_call.hh
+++ src/phantasm-hardware-interface/d3d12/common/safe_seh_call.hh
@@ -4,7 +4,7 @@ typedef struct _EXCEPTION_POINTERS* PEXCEPTION_POINTERS;
 
 namespace phi::d3d12::detail
 {
-[[nodiscard]] bool is_delay_load_exception(PEXCEPTION_POINTERS e);
+bool is_delay_load_exception(PEXCEPTION_POINTERS e);
 
 // Certain calls to DXGI can fail on some Win SDK versions (generally XP or lower) because the call is from a delay-loaded DLL,
 // throwing a Win32 SEH exception. This helper performs such calls safely, using the special __try/__except syntax and an exception filter
@@ -32,4 +32,4 @@ void perform_safe_seh_call(Ft&& f_try, Fe&& f_except)
         f_except();
     }
 }
-}
+} // namespace phi::d3d12::detail
