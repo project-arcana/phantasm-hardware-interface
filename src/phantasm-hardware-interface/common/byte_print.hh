abc3beabe4dfd30d4b99816cc925d2b5c71c2e7b
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2021-08-19 23:38:28 +0200

    Add si_print, fix space in byte_print

--- src/phantasm-hardware-interface/common/byte_print.hh
+++ src/phantasm-hardware-interface/common/byte_print.hh
@@ -15,6 +15,17 @@ inline int byte_print(size_t numBytes, cc::span<char> outString)
     char const* u = " KMGTPEZY";
     while ((l /= 1024.) >= .95 && *u)
         ++u;
-    return snprintf(outString.data(), outString.size(), *u - ' ' ? "%.1f%ciB" : "%.0fB", l * 1024.0, *u);
+    return snprintf(outString.data(), outString.size(), *u - ' ' ? "%.1f %ciB" : "%.0f B", l * 1024.0, *u);
 }
+
+// formats an amount of things into a SI-prefixed version
+inline int si_print(size_t amount, cc::span<char> outString)
+{
+    double l = double(amount);
+    char const* u = " kMGTPEZY";
+    while ((l /= 1000) >= .95 && *u)
+        ++u;
+    return snprintf(outString.data(), outString.size(), *u - ' ' ? "%.1f%c" : "%.0f", l * 1024.0, *u);
+}
+
 } // namespace phi
