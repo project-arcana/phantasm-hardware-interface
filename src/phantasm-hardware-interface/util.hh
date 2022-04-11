8df21cdfa380b21b21a7fa8bfbc3a356947549f8
Jonathan Kunstwald <kunstwald@shapedcode.com>
2022-04-11 20:55:11 +0200

    Improve and clarify texture size utilities

--- src/phantasm-hardware-interface/util.hh
+++ src/phantasm-hardware-interface/util.hh
@@ -33,29 +33,57 @@ inline int get_num_mips(int width, int height) { return int(cc::bit_log2(uint32_
 inline int get_num_mips(tg::isize2 size) { return get_num_mips(size.width, size.height); }
 
 // computes byte size in a GPU buffer to store contents of a texture
-[[deprecated("use get_texture_size_in_buffer, this is wrong for block-compressed and 3D / 2D array cases")]] //
+[[deprecated("use get_texture_size_bytes_on_gpu, this is wrong for block-compressed and 3D / 2D array cases")]] //
 PHI_API uint32_t
 get_texture_size_bytes(tg::isize3 size, format fmt, int num_mips, bool is_d3d12);
 
 // returns the required size for a buffer that holds all subresources of the texture
 // multisampling is ignored
-PHI_API uint32_t get_texture_size_in_buffer(arg::texture_description const& desc, bool is_d3d12);
+PHI_API uint32_t get_texture_size_bytes_on_gpu(arg::texture_description const& desc, bool is_d3d12);
 
-// returns the required size for a buffer that holds a single subresource of the given texture
+struct PHI_API texture_subresource_sizes
+{
+    // row pitch in byte ( = width * bytesPerPixel unless block-compressed)
+    // must be up-aligned to 256B on D3D12 in GPU memory
+    uint32_t pitch_on_disk;
+
+    // amount of rows ( = height unless block-compressed)
+    uint32_t num_rows;
+
+    // amount of 3D depth layers
+    uint32_t num_depths;
+
+    uint32_t get_pitch_on_gpu(bool is_d3d12) const { return is_d3d12 ? cc::align_up(pitch_on_disk, 256) : pitch_on_disk; }
+
+    uint32_t get_size_bytes_on_gpu(bool is_d3d12) const
+    {
+        return num_rows * num_depths * (is_d3d12 ? cc::align_up(pitch_on_disk, 256) : pitch_on_disk);
+    }
+
+    uint32_t get_size_bytes_on_disk() const { return num_rows * num_depths * pitch_on_disk; }
+};
+
+PHI_API texture_subresource_sizes get_texture_subresource_sizes(phi::format fmt, uint32_t width, uint32_t height, uint32_t depth, uint32_t mip_idx);
+
+// returns the required size for a GPU buffer that holds a single subresource of the given texture
 // the MIP index is applied to compute the real width/height/depth
 // NOTE: to store multiple contiguous subresources in a buffer, offsets must be 512 byte aligned
-PHI_API uint32_t get_texture_subresource_size_bytes(phi::format fmt, uint32_t width, uint32_t height, uint32_t depth, uint32_t mip_idx, bool is_d3d12);
+PHI_API uint32_t get_texture_subresource_size_bytes_on_gpu(phi::format fmt, uint32_t width, uint32_t height, uint32_t depth, uint32_t mip_idx, bool is_d3d12);
 
 // computes byte offset in a GPU buffer of the given pixel position in a texture
-PHI_API uint32_t get_texture_pixel_byte_offset(tg::isize2 size, format fmt, tg::ivec2 pixel, bool is_d3d12);
+PHI_API uint32_t get_texture_pixel_byte_offset_on_gpu(tg::isize2 size, format fmt, tg::ivec2 pixel, bool is_d3d12);
 
 // checks if a rowwise texture data copy is in bounds on both input and output memory
 // logs a detailed warning if OOB
 PHI_API bool is_rowwise_texture_data_copy_in_bounds(uint32_t dest_row_stride_bytes, uint32_t row_size_bytes, uint32_t num_rows, uint32_t source_size_bytes, uint32_t destination_size_bytes);
 
 // copies input texture data to destination memory row-by-row, respecting row strides
+// commonly used to upload texture data to a GPU buffer
+// dest_row_stride_bytes: usually GPU pitch (256B-aligned on d3d12)
+// src_row_stride_bytes: usually CPU pitch
+// can be flipped for GPU -> CPU downloads
 // returns num bytes written to dest
-PHI_API uint32_t copy_texture_data_rowwise(void const* __restrict src, void* __restrict dest, uint32_t dest_row_stride_bytes, uint32_t row_size_bytes, uint32_t num_rows);
+PHI_API uint32_t copy_texture_data_rowwise(void const* __restrict src, void* __restrict dest, uint32_t dest_row_stride_bytes, uint32_t src_row_stride_bytes, uint32_t num_rows);
 
 // converts texture data from bgra8 to rgba8
 PHI_API void unswizzle_bgra_texture_data(cc::span<std::byte> in_out_texture_data);
