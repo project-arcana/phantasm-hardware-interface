a11d3afb5ea05d3b8cbf4914c659d3b0fbe96c4b
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2022-04-23 23:32:43 +0200

    Vulkan: Add unused prerequisites for live_command_list

--- src/phantasm-hardware-interface/vulkan/cmd_buf_translation.hh
+++ src/phantasm-hardware-interface/vulkan/cmd_buf_translation.hh
@@ -69,6 +69,10 @@ struct command_list_translator
         _globals.initialize(device, sv_pool, resource_pool, pso_pool, cmd_pool, query_pool, as_pool, has_rt);
     }
 
+    void destroy()
+    { /* TODO */
+    }
+
     void translateCommandList(VkCommandBuffer list, handle::command_list list_handle, vk_incomplete_state_cache* state_cache, std::byte const* buffer, size_t buffer_size);
 
     void execute(cmd::begin_render_pass const& begin_rp);
---
+++ src/phantasm-hardware-interface/vulkan/pools/cmdlist_translator_pool.cc
@@ -0,0 +1,73 @@
+#include "cmdlist_translator_pool.hh"
+
+#include <phantasm-hardware-interface/vulkan/cmd_buf_translation.hh>
+
+void phi::vk::CmdlistTranslatorPool::initialize(VkDevice device,
+                                                ShaderViewPool* sv_pool,
+                                                ResourcePool* resource_pool,
+                                                PipelinePool* pso_pool,
+                                                CommandListPool* cmd_pool,
+                                                QueryPool* query_pool,
+                                                AccelStructPool* as_pool,
+                                                bool has_rt,
+                                                cc::allocator* pStaticAlloc,
+                                                uint32_t maxNumTranslators)
+{
+    mPool.initialize(maxNumTranslators, pStaticAlloc);
+
+    mTranslators.reset(pStaticAlloc, maxNumTranslators);
+
+    for (auto i = 0u; i < maxNumTranslators; ++i)
+    {
+        auto* const pNewTranslator = pStaticAlloc->new_t<command_list_translator>();
+        pNewTranslator->initialize(device, sv_pool, resource_pool, pso_pool, cmd_pool, query_pool, as_pool, has_rt);
+        mTranslators[i] = pNewTranslator;
+    }
+
+    mBackingAlloc = pStaticAlloc;
+}
+
+void phi::vk::CmdlistTranslatorPool::destroy()
+{
+    mPool.iterate_allocated_nodes([](Node& node) {
+        // TODO
+        // node.pTranslator->endTranslation(true);
+    });
+
+    for (auto* const pTranslator : mTranslators)
+    {
+        pTranslator->destroy();
+        mBackingAlloc->delete_t(pTranslator);
+    }
+}
+
+phi::handle::live_command_list phi::vk::CmdlistTranslatorPool::createLiveCmdList(handle::command_list backing,
+                                                                                 VkCommandBuffer pRawCmdBuf,
+                                                                                 queue_type queue,
+                                                                                 vk_incomplete_state_cache* pStateCache,
+                                                                                 cmd::set_global_profile_scope const* pOptGlobalProfileScope)
+{
+    CC_ASSERT(!mPool.is_full() && "Maximum amount of live commandlists reached - increase max_num_live_commandlists in config");
+
+    auto res = mPool.acquire();
+
+    Node& node = mPool.get(res);
+    node.hBackingList = backing;
+    node.pTranslator = mTranslators[mPool.get_handle_index(res)];
+
+    // TODO
+    // node.pTranslator->beginTranslation(pRawList, queue, pStateCache, pOptGlobalProfileScope);
+
+    return {res};
+}
+
+phi::handle::command_list phi::vk::CmdlistTranslatorPool::freeLiveCmdList(handle::live_command_list list, bool bDoClose)
+{
+    // TODO
+    // this->getTranslator(list)->endTranslation(bDoClose);
+    auto const res = this->getBackingList(list);
+
+    mPool.release(list._value);
+
+    return res;
+}
