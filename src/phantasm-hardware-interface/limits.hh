deed8a90e0c4f0329aecd35dfaf8e18ece5be5d5
Jonathan Kunstwald <mail@jonathankunstwald.de>
2025-05-23 01:10:59 +0200

    Limits: Remove unused max_shader_samplers

--- src/phantasm-hardware-interface/limits.hh
+++ src/phantasm-hardware-interface/limits.hh
@@ -21,10 +21,6 @@ enum limits_e : unsigned
     /// most non-desktop GPUs only support a maximum of 8.
     max_shader_arguments = 4u,
 
-    /// the maximum amount of samplers per shader view
-    /// configurable
-    max_shader_samplers = 16u,
-
     /// the maximum size for root constants
     /// configurable in increments of 4, also concerns CPU memory (cmd::draw, cmd::dispatch)
     max_root_constant_bytes = 16u,
